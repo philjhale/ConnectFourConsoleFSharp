373380b74173f5229038e5b3066943c06fde5c7f
Phil Hale <philip.hale@tombola.com>
2015-05-25 20:58:21 +0100

    Shifted game functions into Game

--- ConnectFourFSharp/Game.fs
+++ ConnectFourFSharp/Game.fs
@@ -1,2 +1,41 @@
 ﻿module Game
 
+open Board
+open Player
+
+    let (|Int|_|) str =
+        match System.Int32.TryParse(str) with
+        | (true,int) -> Some(int)
+        | _ -> None
+
+    let rec getValidColumnNumberInput getColumnNumberInput =
+        match getColumnNumberInput() with
+            | Int i -> i
+            | _ -> 
+                printfn "Please enter an integer"
+                getValidColumnNumberInput getColumnNumberInput
+
+    let rec getValidDrop drop =
+        match drop with
+        | Success board -> board
+        | OutOfBounds board ->
+            printf "That column is out of bounds, please choose another"
+            getValidDrop drop
+
+    let rec takeTurn board players =
+        // Another way. Pass in current player to takenTurn
+        // Recursive call is players |> List.find (fun player -> player.Name <> currentPlayer.Name)
+        let currentPlayer = List.nth players 0
+
+        printfn "%s, enter a column number:" currentPlayer.Name
+        
+        let validColumnNumber = getValidColumnNumberInput <| Player.getInput currentPlayer.Type
+        let newBoard = getValidDrop <| Board.drop validColumnNumber board currentPlayer.Disc
+        
+        Board.showBoard newBoard
+
+        // TODO Check for connect four
+        // TODO Handle out of bounds input. E.g. Column 100
+        match Board.isConnectFour newBoard validColumnNumber with
+        | true -> printf "%s wins!" currentPlayer.Name
+        | false -> takeTurn newBoard <| List.rev players
--- ConnectFourFSharp/Program.fs
+++ ConnectFourFSharp/Program.fs
@@ -1,6 +1,7 @@
 ﻿module Program
 // Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
+open Game
 open Board
 open Player
 open System
@@ -24,44 +25,6 @@ open System
 
 [<EntryPoint>]
 let main argv = 
-    let (|Int|_|) str =
-        match System.Int32.TryParse(str) with
-        | (true,int) -> Some(int)
-        | _ -> None
-
-    let rec getValidColumnNumberInput getColumnNumberInput =
-        match getColumnNumberInput() with
-            | Int i -> i
-            | _ -> 
-                printfn "Please enter an integer"
-                getValidColumnNumberInput getColumnNumberInput
-
-    let rec getValidDrop drop =
-        match drop with
-        | Success board -> board
-        | OutOfBounds board ->
-            printf "That column is out of bounds, please choose another"
-            getValidDrop drop
-
-    let rec takeTurn board players =
-        // Another way. Pass in current player to takenTurn
-        // Recursive call is players |> List.find (fun player -> player.Name <> currentPlayer.Name)
-        let currentPlayer = List.nth players 0
-
-        printfn "%s, enter a column number:" currentPlayer.Name
-        
-        let validColumnNumber = getValidColumnNumberInput <| Player.getInput currentPlayer.Type
-        let newBoard = getValidDrop <| Board.drop validColumnNumber board currentPlayer.Disc
-        
-        Board.showBoard newBoard
-
-        // TODO Check for connect four
-        // TODO Handle out of bounds input. E.g. Column 100
-        match Board.isConnectFour newBoard validColumnNumber with
-        | true -> printf "%s wins!" currentPlayer.Name
-        | false -> takeTurn newBoard <| List.rev players
-
-
     printfn "Game started"
     let human = Player.create "Phil" Disc.Red PlayerType.Human
     let computer = Player.create "Computer" Disc.Yellow PlayerType.Computer
